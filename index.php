3cd87f239ff513c1e44976935239714fc1ac90a5
mighty-mite <tushinov.ayur@gmail.com>
2024-10-17 22:59:48 +0300

    ep 16 connect mysql

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
-require 'functions.php';
+$dsn = "mysql:host=mysql-8.2.local;port=3306;dbname=demo;charset=utf8mb4";
 
-require 'router.php';
+$pdo = new PDO($dsn, 'root', '');
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($posts);
+
+// require 'functions.php';
+
+// require 'router.php';
+
