55f41d07f774cdcfaa15429879aafe38bcc52516
mighty-mite <tushinov.ayur@gmail.com>
2024-10-17 18:53:39 +0300

    ep 15 router

---
+++ router.php
@@ -0,0 +1,34 @@
+<?php
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+if ($uri === '/') {
+  require './controllers/index.php';
+} else if ($uri == '/about') {
+  require './controllers/about.php';
+} else if ($uri == '/contact') {
+  require './controllers/contact.php';
+}
+
+$routes = [
+  '/' => './controllers/index.php',
+  '/about' => './controllers/about.php',
+  '/contact' => './controllers/contact.php',
+];
+
+function routeToContoller($uri, $routes)
+{
+  if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+  } else {
+    abort();
+  }
+}
+
+function abort()
+{
+  http_response_code((404));
+  echo '404';
+  die();
+}
+
+routeToContoller($uri, $routes);
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -10,12 +10,12 @@
           <div class="ml-10 flex items-baseline space-x-4">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             <a href="/"
-              class="<?php echo $_SERVER['REQUEST_URI'] == '/' ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> rounded-md px-3 py-2 text-sm font-medium "
+              class="<?= isUrl('/') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> rounded-md px-3 py-2 text-sm font-medium "
               aria-current="page">Home</a>
-            <a href="/about.php"
-              class="<?php echo $_SERVER['REQUEST_URI'] == '/about.php' ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> rounded-md px-3 py-2 text-sm font-medium ">About</a>
-            <a href="/contact.php"
-              class="<?php echo $_SERVER['REQUEST_URI'] == '/contact.php' ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> rounded-md px-3 py-2 text-sm font-medium ">Contact</a>
+            <a href="/about"
+              class="<?= isUrl('/about') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> rounded-md px-3 py-2 text-sm font-medium ">About</a>
+            <a href="/contact"
+              class="<?= isUrl('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> rounded-md px-3 py-2 text-sm font-medium ">Contact</a>
           </div>
         </div>
       </div>
